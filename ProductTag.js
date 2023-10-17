e0db42fffc749910570ae09e4add110366487c01
khalilb2000 <khalilb2000@gmail.com>
2023-10-17 07:58:33 -0500

    adding in more id's for the tags and products, also adding in references for each table

--- ProductTag.js
+++ ProductTag.js
@@ -7,6 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTGER, // setting Data type to INTEGER
+      allowNull: false, // Disallow NULL values
+      primaryKey: true, // Set as primary key 
+      autoIncrement: true, // Automatically increment the value for new redord
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', //Reference the 'product' table
+      }
+    }
   },
   {
     sequelize,
