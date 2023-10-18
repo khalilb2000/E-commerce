c6b39425e19ecad0a9f8f503f2621d459acaf013
khalilb2000 <khalilb2000@gmail.com>
2023-10-18 01:36:38 -0500

    completing delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,18 +35,36 @@ router.get('/:id', async (req, res) => {
 router.post('/',async (req, res) => {
   // create a new tag
   try {
-    con
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(400).json({ message: 'Tag creation failed' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const update = await Tag.update(req.body, {
+      where: { id: req.params.id },
+   });
+   !updated[0]
+   ? res.status(404).json({ message: "No tag found with this id!"})
+   :res.status(200).json(updated);
+  } catch (error) {
+    res.status(500).json({ message: 'Tag updated failed' });
+  }
+});
+
+router.delete('/:id',async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    
   } catch (error) {
     
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
 
 module.exports = router;
